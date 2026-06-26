dbcca2d8c0d8e869afffb340bb33391a3f023214
Angelo Greison <83356715+angelogreison@users.noreply.github.com>
2026-06-26 14:09:19 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'artesana-del-barro-v5';
+const CACHE_NAME = 'artesana-del-barro-v6';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -6,9 +6,9 @@ const ASSETS_TO_CACHE = [
   './css/schedule.css',
   './js/main.js',
   './js/schedule.js',
-  './img/hero_bg.webp',
+  './img/Clases%20de%20Cer%C3%A1mica%20en%20Caballito%20y%20Almagro%20-%20Taller%20de%20Alfarer%C3%ADa%20y%20Modelado.webp',
   './logo.webp',
-  './logo-letras.webp',
+  './Artesana-del-barro.webp',
   './logo-footer.webp',
   './wa-white.webp'
 ];
